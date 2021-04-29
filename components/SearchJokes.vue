c371721abf5faf6a7e1c2a3e5ba792527858dc9d
Al Mustarik <almustarik@gmail.com>
2021-04-29 16:08:34 +0600

    added all fucntionality

---
+++ components/SearchJokes.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="obSubmit">
+        <input type="text" v-model="text" placeholder="Search Jokes..">
+        <input type="submit" value="Search Jokes">
+    </form>
+</template>
+
+<script>
+export default {
+    name: "SearchJokes",
+    data() {
+        return {
+            text: "",
+        };
+    },
+    methods: {
+        obSubmit() {
+            this.$emit("search-text", this.text);
+            this.text = "";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Welcome to the best corny dad jokes app on the web.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+      return {
+        title: 'The App',
+          meta: [
+            {
+              hid: 'description',
+              name: 'description',
+              content: 'Best place for corny dad jokes'
+            }
+          ]
+        }
+      }
+   }
+</script>
+
+<style>
+
+</style>
